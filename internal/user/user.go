d756e11629b887911b02db848575d1f7e777130a
Ragil Burhanudin Pamungkas <sipamungkas.dev@gmail.com>
2025-09-18 16:14:23 +0700

    feat(user): define User model with GORM and unique ID generation

--- internal/user/user.go
+++ internal/user/user.go
@@ -1,1 +1,26 @@
 package user
+
+import (
+	"time"
+
+	"github.com/lucsky/cuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	ID        string         `gorm:"primaryKey" json:"id"`
+	Name      string         `gorm:"not null" json:"name"`
+	Email     string         `gorm:"uniqueIndex" json:"email"`
+	Password  string         `json:"password"`
+	NIP       string         `gorm:"uniqueIndex; NOT NULL" json:"nip"`
+	Status    string         `json:"status"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	u.ID = cuid.New()
+	return
+}
